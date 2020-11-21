8517a8778a057a56362045358b89682cef81ecbd
Nestuna <nicolasantunespereira@gmail.com>
2020-11-21 20:28:41 +0100

    refactoring cast functions

--- CodingGame_FallChallenge/Player.java
+++ CodingGame_FallChallenge/Player.java
@@ -16,16 +16,28 @@ class Player {
     static ArrayList<List<Integer>> spellsSavedList = new ArrayList<List<Integer>> () ;
     static Boolean spellsAreSaved = false; 
 
+    // UTILS ---------------------------------------
     public static void saveSpells(List<Integer> spell) {
         spellsSavedList.add(spell);
     }
 
-    public static String actionToMake(ArrayList<List<Integer>> commands, List<Integer> inventoryPlayer) {
-        int commandId;
+
+    // ACTION & COMMAND ---------------------------------------
+    public static String actionToMake(ArrayList<List<Integer>> commands, List<Integer> inventoryPlayer, ArrayList<List<Integer>> spellsList) {
         String action;
         List<Integer> command = mostRupees(commands);
-        if (!possibleCommand(command, inventoryPlayer)) {
-            action = castCommand(command, inventoryPlayer);
+        int commandId = command.get(0);
+        List<Integer> commandItems = new ArrayList<Integer>();
+
+        // On copie les items, sans l'actionId et le prix pour plus de faciliter
+        for (int i=2; i < command.size(); i++) {
+            commandItems.add(command.get(i));
+        }
+
+        System.err.println("Inventaire :" + inventoryPlayer);
+        if (!inventoryIsGood(commandItems, inventoryPlayer)) {
+            System.err.println("Commande pas possible !");         
+            action = castCommand(commandItems, inventoryPlayer, spellsList);
         } else {
             action = "BREW " + commandId;
         }
@@ -34,49 +46,70 @@ class Player {
 
     public static List<Integer> mostRupees(ArrayList<List<Integer>> commands) {
         List<Integer> mostProfitableCommand = commands.get(0);
-        int higherPrice = commands.get(0).get(5);
+        int higherPrice = commands.get(0).get(1);
         for(List<Integer> command : commands)
-            if(command.get(5) > higherPrice) {
-                higherPrice = command.get(5);
+            if(command.get(1) > higherPrice) {
+                higherPrice = command.get(1);
                 mostProfitableCommand = command;
             }
         System.err.println("Commande la plus rentable :" + mostProfitableCommand);
         return mostProfitableCommand;
     }
 
-    public static boolean possibleCommand(List<Integer> command, List<Integer> inventory) {
-        for (int i = 1; i < command.size() - 1; i++) {
-            if ((inventory.get(i) + command.get(i)) < 0) {
+    public static boolean inventoryIsGood(List<Integer> itemList, List<Integer> inventory) {
+        for (int i = 0; i < itemList.size(); i++) {
+            if ((inventory.get(i) + itemList.get(i)) < 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public static String castCommand(List<Integer> command, List<Integer> inventory) {
+    // CAST ---------------------------------------
+    public static String castCommand(List<Integer> command, List<Integer> inventory, ArrayList<List<Integer>> spellsList) {
         List<Integer> itemsToCast = findItemsToCast(command, inventory);
-        int spellToCast = findSpellToCast(itemsToCast);
-        String castAction;
+        int spellToCast = findSpellToCast(itemsToCast, spellsList);
+        String castAction = "BREW " + spellToCast;
 
         return castAction;
     }
 
-    public static List<Integer> findItemsToCast(List<Integer> command, List<Integer> inventory) {
+    public static List<Integer> findItemsToCast(List<Integer> itemsList, List<Integer> inventory) {
         List<Integer> itemsToCast = new ArrayList<Integer>(Arrays.asList(0,0,0,0));
-        for (int i = 1; i < command.size() - 1; i++) {
-            if ((inventory.get(i) + command.get(i)) < 0) {
-                itemsToCast.set(i-1, inventory.get(i) + command.get(i));
+        for (int i = 0; i < itemsList.size(); i++) {
+            if ((inventory.get(i) + itemsList.get(i)) < 0) {
+                itemsToCast.set(i, inventory.get(i) + itemsList.get(i));
             }
         }
         System.err.println("Items à caster:" + itemsToCast);
         return itemsToCast;
     }
 
-    public static int findSpellToCast(List<Integer> items) {
-        int spellToCast;
+    public static int findSpellToCast(List<Integer> items, ArrayList<List<Integer>> spellsList) {
+        int spellToCast = 0;
+        int itemsInSpell = 0;
+        // for (List<Integer> spell : spellsSavedList) {
+        //     for (int i = 0; i < spell.size(); i++) {
+        //         if (items[i] = spell[i])
+        //     }
+        // }
+
+        if (spellIsAvailable(78, spellsList)) {
+            spellToCast = 1;
+        }
+
 
         System.err.println("Sort à caster:" + spellToCast);
         return spellToCast;
+    }
+
+    public static boolean spellIsAvailable (int spellId, ArrayList<List<Integer>> spellsList) {
+        for (List<Integer> spell : spellsList) {
+            if (spellId == spell.get(0)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String args[]) {
@@ -85,7 +118,7 @@ class Player {
         // game loop
         while (true) {
             int actionCount = in.nextInt(); // the number of spells and recipes in play
-            System.err.println("actionCount :" + actionCount);
+            // System.err.println("actionCount :" + actionCount);
 
             // INIT VARIABLES
             ArrayList<List<Integer>> commandsList = new ArrayList<List<Integer>>();
@@ -93,6 +126,8 @@ class Player {
             ArrayList<List<Integer>> spellsOpponentList = new ArrayList<List<Integer>>();
             List<Integer> inventoryPlayerList = new ArrayList<Integer>();
             List <Integer> inventoryOpponentList = new ArrayList<Integer>();
+            int scorePlayer;
+            int scoreOpponent;
             spellsAreSaved = spellsSavedList.size() > 0; // false au début du jeu
 
             for (int i = 0; i < actionCount; i++) {
@@ -125,7 +160,7 @@ class Player {
                     }
 
                 } else {
-                    List<Integer> command = Arrays.asList(actionId, delta0, delta1, delta2, delta3, price);
+                    List<Integer> command = Arrays.asList(actionId, price, delta0, delta1, delta2, delta3);
                     commandsList.add(command);
                 }
                
@@ -140,13 +175,15 @@ class Player {
                 int inv3 = in.nextInt();
                 int score = in.nextInt(); // amount of rupees
                 if (i==0) {
-                    inventoryPlayerList = Arrays.asList(inv0, inv1, inv2, inv3, score);
+                    inventoryPlayerList = Arrays.asList(inv0, inv1, inv2, inv3);
+                    scorePlayer = score;
                 } else if (i==1) {
-                    inventoryOpponentList = Arrays.asList(inv0, inv1, inv2, inv3, score);
+                    inventoryOpponentList = Arrays.asList(inv0, inv1, inv2, inv3);
+                    scoreOpponent = score;
                 }
 
             }
-            
+
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
             System.err.println("Sorts sauvegardés: " + spellsSavedList);
@@ -154,8 +191,8 @@ class Player {
             System.err.println("Sorts Adversaire disponible: " + spellsPlayerList);
             
             // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-            String action = actionToMake(commandsList, inventoryPlayerList));
-            System.out.println("BREW " + action );
+            String action = actionToMake(commandsList, inventoryPlayerList, spellsPlayerList);
+            System.out.println(action);
         }
     }
 }
