c44a96db474d79e63b1fdef24ff2d93d247dfd8e
Nestuna <nicolasantunespereira@gmail.com>
2020-12-27 15:03:42 +0100

    add positions methods & ordering player & opponent mapping

--- CodingGame_Tron/Player.java
+++ CodingGame_Tron/Player.java
@@ -20,29 +20,65 @@ class Robot {
         this.curX = 0;
         this.curY = 0;
     }
-    Robot(int x0, int y0) {
+
+    Robot (int x0, int y0, int x1, int y1) {
         this.initX = x0;
         this.initY = y0;
+        this.curX = x1;
+        this.curY = y1;
     }
 
-    public int getDistanceX() {
+    // ------------------------------ Coordonates
+    public int getX() {
+        return this.curX;
+    }
+
+    public int getY() {
+        return this.curY;
+    }
+
+    public void setX(int x) {
+        this.curX = x;
+    }
+
+    public void setY(int y) {
+        this.curY = y;
+    }
+
+    public int[] getPosition() {
+        int[] position = {this.curX, this.curY}; 
+        return position;
+    }
+
+    public void setPosition(int x1, int y1) {
+        this.curX = x1;
+        this.curY = y1;
+    }
+
+    public void setPosition(int x0, int y0, int x1, int y1) {
+        this.initX = x0;
+        this.initY = y0;
+        this.curX = x1;
+        this.curY = y1;
+    }
+
+    // ------------------------------ Distances
+    public int getDistanceFromX(int x) {
+        return Math.abs(this.curX - x);
+    }
+
+    public int getDistanceFromY(int y) {
+        return Math.abs(this.curY - y);
+    }
+
+    public int getDistanceFromInitX() {
         return Math.abs(this.initX - this.curX);
     }
-    public int getDistanceY() {
+
+    public int getDistanceFromInitY() {
         return Math.abs(this.initY - this.curY);
     }
 
-    public void setPositions(int x1, int y1) {
-        this.curX = x1;
-        this.curY = y1;
-    }
-
-    public void setPositions(int x0, int y0, int x1, int y1) {
-        this.initX = x0;
-        this.initY = y0;
-        this.curX = x1;
-        this.curY = y1;
-    }
 
     @Override
     public String toString() {
@@ -56,6 +92,10 @@ class OpponentRobot extends Robot {
         super();
     }
 
+    OpponentRobot(int x0, int y0, int x1, int y1) {
+        super (x0, y0, x1, y1);
+    }
+
 }
 
 class PlayerRobot extends Robot {
@@ -64,15 +104,21 @@ class PlayerRobot extends Robot {
         super();
     }
 
-        
+    PlayerRobot(int x0, int y0, int x1, int y1) {
+        super(x0, y0, x1, y1);
+    }
+
+    public String move() {
+        return "LEFT";
+    }
 }
 
 
 class Player {
     public static void main(String args[]) {
-        
-        Robot playerRobot = new PlayerRobot();
-        Robot opponentRobot = new OpponentRobot();
+        // Robots
+        PlayerRobot player = null;
+        HashMap<Integer, Robot> opponentsMap = new HashMap<Integer, Robot>();
 
         Scanner in = new Scanner(System.in);
 
@@ -85,24 +131,34 @@ class Player {
                 int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
                 int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
                 int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
-
+                
                 if (i == P) {
-                    playerRobot.setPositions(X0,Y0,X1,Y1);
+                    if (player == null) {
+                        player = new PlayerRobot(X0,Y0,X1,Y1);
+                    } else {
+                        player.setPosition(X1,Y1);
+                    }
                 } else {
-                    opponentRobot.setPositions(X0, Y0, X1, Y1);
+                    if (opponentsMap.size() < N - 1) {
+                        Robot opponent = new OpponentRobot(X0,Y0,X1,Y1);
+                        opponentsMap.put(i, opponent); 
+                    } else {
+                        Robot opponent = opponentsMap.get(i);
+                        opponent.setPosition(X1, Y1);
+                        opponentsMap.put(i, opponent);    
+                    }
                 }
             }
 
             // DEBUG
             System.err.println("Player :");
-            System.err.println(playerRobot);
+            System.err.println(player);
             System.err.println("Opponent :");
-            System.err.println(opponentRobot);
+            System.err.println(opponentsMap);
             
 
-
             // ACTION
-            System.out.println("UP"); // A single line with UP, DOWN, LEFT or RIGHT
+            System.out.println(player.move()); // A single line with UP, DOWN, LEFT or RIGHT
         }
     }
 }
